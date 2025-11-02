94634746c745f331658b4980a911bace7e35b370
fatihayyildiz <fatihhikmetayyildiz@gmail.com>
2025-11-02 17:15:58 +0100

    refactor: update pom.xml for Java 8 compatibility and add configuration for maven-surefire-plugin; simplify ExampleTest class

--- src/test/java/example/ExampleTest.java
+++ src/test/java/example/ExampleTest.java
@@ -5,16 +5,6 @@ import com.intuit.karate.junit5.Karate;
 class ExampleTest {
 
     @Karate.Test
-    Karate testPodcast() {
-        return Karate.run("Podcast").relativeTo(getClass());
-    }
-
-    @Karate.Test 
-    Karate testToken() {
-        return Karate.run("Token").relativeTo(getClass());
-    }
-
-    @Karate.Test
     Karate testAll() {
         return Karate.run("classpath:example");
     }
